4a86c909b642128395f462c270bee916ef99452a
Oksil-Joo <bbkke07@gmail.com>
2021-10-30 15:54:48 -0400

    uploadPHPfiles

--- js/main.js
+++ js/main.js
@@ -1,36 +1,25 @@
+import{ getData } from "./components/TheDataMiner.js";
+import TheThumbNail from "./components/TheThumbNail.js";
+
 (() => {
-    const   theTeam = document.querySelector("#team-members"),
-            theTemplate = document.querySelector("#bio-template").content;
-  
-          
-    function getData() {
-        fetch("./data.json")
-        .then(res => res.json()) //unpach the API
-        .then(data => {
-            console.table(data);
-        
-            buildTeam(data);
-        })
-        .catch(error => console.error(error));
-    }
+    const myVue = new Vue({
+        created: function() {
+            // go fetch the portfolio data here
+            getData(null, (data) => this.portfolioData = data )
+        },
 
+        data: {
+            // this is storing the database info from our fetch call
+            portfolioData: [],
+            message: "hello from Vue"
+        },
 
-    function buildTeam(info) {
-       let favor = Object.keys(info);
+        methods: {
 
-       favor.forEach(person => {
-           let panel = theTemplate.cloneNode(true),
-                memberInfo = panel.firstElementChild.children;
+        },
 
-                memberInfo[0].querySelector('img').src = `images/${info[person].biopic}`;
-                memberInfo[1].textContent = info[person].name;
-                memberInfo[2].textContent = info[person].role;
-                memberInfo[3].textContent = info[person].nickname;
-
-            theTeam.appendChild(panel);
-       })
-    }
-
-    getData();
-
-})();
+        components: {
+            thumb: TheThumbNail
+        }
+    }).$mount("#app");    
+})()
