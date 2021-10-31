d7c0fd5d031adfc87fc8484758bc537c45333b0b
Oksil-Joo <bbkke07@gmail.com>
2021-10-30 20:33:58 -0400

    modifieCSSStyle

--- includes/functions.php
+++ includes/functions.php
@@ -1,16 +1,13 @@
 <?php
-    // store the processed results in a variable
     $result = array();
 
-    // if a user passes an ID via a query string (?id=1)
-    // then we should retrieve the row of data that matches and pass it back to the app
     function getOneProfs($conn, $prof) {
    
         $query = "SELECT * FROM profs WHERE id='".$prof."'";
-    // this is the database result -> the raw data that SQL gives us
+
     $runQuery = $conn->query($query);
 
-    // process our DB result and make something we can use with AJAX
+
     while($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
      $result[] = $row;
     }
@@ -22,10 +19,9 @@
    
             $query = "SELECT * FROM profs";
 
-        // this is the database result -> the raw data that SQL gives us
+
         $runQuery = $conn->query($query);
 
-        // process our DB result and make something we can use with AJAX
         while($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
          $result[] = $row;
         }
