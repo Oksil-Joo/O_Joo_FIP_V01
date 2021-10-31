56869808cf659a0561fbc727c1fdcf310aaadb80
Oksil-Joo <bbkke07@gmail.com>
2021-10-30 20:49:01 -0400

    modifiedPHPfile

--- includes/functions.php
+++ includes/functions.php
@@ -1,23 +1,13 @@
 <?php
+
     $result = array();
 
-    function getOneProfs($conn, $prof) {
-   
-        $query = "SELECT * FROM profs WHERE id='".$prof."'";
-
-    $runQuery = $conn->query($query);
-
-
-    while($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
-     $result[] = $row;
-    }
-
-    return $result;
-}
-  
-    function getAllprofs($conn) {
-   
+    function getProfData($conn, $prof) {
+        if (is_null($prof)) {
             $query = "SELECT * FROM profs";
+        } else {
+            $query = "SELECT * FROM profs WHERE id='".$prof."'";
+        }
 
 
         $runQuery = $conn->query($query);
