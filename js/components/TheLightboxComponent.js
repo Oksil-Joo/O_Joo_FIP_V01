b1313872deacc431cb5a040c18c2eca747dc2bd6
Oksil-Joo <bbkke07@gmail.com>
2021-12-06 01:56:32 -0500

    modifyVidoeComponent

--- js/components/TheLightboxComponent.js
+++ js/components/TheLightboxComponent.js
@@ -1,24 +1,36 @@
+import TheVideoComponent from "./TheVideoComponent.js";
+
 
 export default {
     name: "TheLightboxComponent",
 
     props: ["piece"],
 
+    computed: {
+        activeComponent: function() {
+            return `${this.piece.mediaType + "Component"}`;
+        }
+    },
+
     template:
     `<section class="lightboxWrapper">
-        <div class=""wrapCon>
-            <div class="btAlign">
-                <i @click="closeMe" class="fa fa-window-close" style="font-size:48px;color:#E96A97"></i>
+
+        <div class="btAlign">
+            <i @click="closeMe" class="fa fa-window-close" style="font-size:48px;color:#E96A97"></i>
+        </div>
+        <div class="wrapCon">
+            <div class="dataImg">
+                <img :src='"images/" + piece.bigImg' :alt="piece.name">
+                <component v-if="piece.mediaType" :is="activeComponent"></component>
             </div>
-            <div>
-                <img :src='"images/" + piece.bigimg' :alt="piece.name">
+            <div class="wrapDes">
                 <h1 class="title" > {{piece.name}} </h1>
                 <h2 class="subTitle"> {{piece.role}}  </h2>
                 <h2 class="lightTitle">concept</h2>
                 <p class="lightContents"> {{piece.description}} </p>
                 <h2 class="lightTitle">Using Software</h2>
                 <p class="lightContents"> {{piece.software}} </p>
-            <div>
+            </div>
         <div>
     </section>
     `,
@@ -27,5 +39,9 @@ export default {
         closeMe() {
             this.$emit("closelb");
         }
+    },
+
+    components: {
+        videoComponent: TheVideoComponent
     }
 }
---
+++ js/components/TheVideoComponent.js
@@ -0,0 +1,11 @@
+export default {
+    name: "TheVideoComponent",
+
+    template: `
+        <section class="video-player">
+        <i class="fa fa-youtube-play" style="font-size:48px;color:red"></i>
+        <video src="video/morphVideo.mp4" controls></video>
+            <p> test </p>
+        <section>
+    `
+}
