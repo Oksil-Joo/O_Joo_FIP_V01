20e2e3997ebda66298f17cdbe16bc06ba3d0cb41
Oksil-Joo <bbkke07@gmail.com>
2021-12-05 23:17:41 -0500

    updatePortfolio

--- js/components/TheLightboxComponent.js
+++ js/components/TheLightboxComponent.js
@@ -6,11 +6,19 @@ export default {
 
     template:
     `<section class="lightboxWrapper">
-        <div>
-            <i @click="closeMe" class="fa fa-window-close" style="font-size:48px;color:#E96A97"></i>
-            <h1 class="title" > {{piece.name}} </h1>
-            <h2 class="subTitle" > {{piece.role}}  </h2>
-            <p class="lightContents" > {{piece.description}} </p>
+        <div class=""wrapCon>
+            <div class="btAlign">
+                <i @click="closeMe" class="fa fa-window-close" style="font-size:48px;color:#E96A97"></i>
+            </div>
+            <div>
+                <img :src='"images/" + piece.bigimg' :alt="piece.name">
+                <h1 class="title" > {{piece.name}} </h1>
+                <h2 class="subTitle"> {{piece.role}}  </h2>
+                <h2 class="lightTitle">concept</h2>
+                <p class="lightContents"> {{piece.description}} </p>
+                <h2 class="lightTitle">Using Software</h2>
+                <p class="lightContents"> {{piece.software}} </p>
+            <div>
         <div>
     </section>
     `,
